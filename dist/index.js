ccd2370e3ba1b4a3c26b19895673b79926117d3d
Mohamed Bennekrouf <mohamed.bennekrouf@gmail.com>
2023-10-15 19:59:50 +0200

    Update lib

--- dist/index.js
+++ dist/index.js
@@ -13,6 +13,11 @@ var __createBinding = (this && this.__createBinding) || (Object.create ? (functi
 var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./ErrorBoundaries"), exports);
+exports.ErrorBoundary = void 0;
 __exportStar(require("./Logger"), exports);
+var ErrorBoundaries_1 = require("./ErrorBoundaries");
+Object.defineProperty(exports, "ErrorBoundary", { enumerable: true, get: function () { return __importDefault(ErrorBoundaries_1).default; } });
